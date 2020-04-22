27ee5bb62d95b4661e19a94a226b62bac6b42491
GeovanniAlderete <geovanni.alderete.ruano.201@gmail.com>
2020-04-21 21:30:24 -0500

    Actualizacion json header

--- StackJobs/controllers/app.js
+++ StackJobs/controllers/app.js
@@ -5,8 +5,8 @@ app.controller('MaincCtrl', ['$scope', function($scope) {
     // SET:GET HEAD MAINCTRL
     $scope.headerMain = {};
     $scope.headerMain = {
-        "title": "StackJobs México",
-        "contentTitle": "StackJobs"
+        title: "StackJobs México",
+        contentTitle: "StackJobs"
     }
 
 }]);
